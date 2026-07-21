9484b196215dce662483c60ca04491f3be584f8f
ramitha <ramithamanilka2003@gmail.com>
2026-07-21 17:17:52 +0530

    setting page

--- app/(main)/settings/page.tsx
+++ app/(main)/settings/page.tsx
@@ -1,19 +1,11 @@
-import { auth } from "@/lib/auth"
-import { headers } from "next/headers"
-
-export default async function SettingsPage() {
-
-    const session = await auth.api.getSession({
-        headers: await headers()
-    })
+export default function SettingsPage() {
 
     return (
         <div>
             <h1 className="text-center font-semibold text-3xl">Settings</h1>
 
 
-            <h1>{session?.user.name}</h1>
-            <h1>{session?.user.email}</h1>
+            <h1>Page is in construnction</h1>
         </div>
     )
 }
