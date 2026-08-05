a5b1159c00cfd85ba191f4d766f7b103287d5034
ramitha <ramithamanilka2003@gmail.com>
2026-08-05 19:31:52 +0530

    implemented backend logic for password reset

--- lib/actions/auth-actions.ts
+++ lib/actions/auth-actions.ts
@@ -3,6 +3,10 @@
 import { headers } from "next/headers"
 import { auth } from "../auth"
 import { redirect } from "next/navigation"
+import prisma from "../prisma"
+import { randomInt } from "crypto"
+import { transporter } from "../utils/mailsender"
+import bcrypt from "bcryptjs"
 
 export const signIn = async (email: string, password: string) => {
     try {
@@ -97,3 +101,159 @@ export const signOut = async () => {
     return response
 }
 
+export const requestPasswordReset = async (email: string) => {
+
+    try {
+
+        const user = await prisma.user.findUnique({
+            where: { email }
+        })
+
+        if (!user) {
+            return {
+                success: false,
+                message: "User not found"
+            }
+        }
+
+        const code = randomInt(10000, 99999).toString()
+
+        await prisma.verification.deleteMany({
+            where: {
+                identifier: email
+            }
+        })
+
+        await prisma.verification.create({
+            data: {
+                id: crypto.randomUUID(),
+                identifier: email,
+                value: code,
+                expiresAt: new Date(Date.now() + 1000 * 60 * 10)
+            }
+        })
+
+        await transporter.sendMail({
+            from: `"Clothique" <${process.env.EMAIL}>`,
+            to: email,
+            subject: "Reset Your Password",
+            html: `
+                <h2>Password Reset Request</h2>
+                <p>Your verification code is:</p>
+                <h1>${code}</h1>
+                <p>This code will expire in 10 minutes.</p>
+            `
+        })
+
+        return {
+            status: "success",
+            message: "Reset code sent successfully"
+        }
+    } catch (error) {
+
+        console.log(error)
+
+        return {
+            status: "error",
+            message: "Failed to send reset code"
+        }
+    }
+}
+
+
+export const verifyResetCode = async (email: string, code: string) => {
+
+    try {
+
+        const record = await prisma.verification.findFirst({
+            where: {
+                identifier: email,
+                value: code
+            }
+        })
+
+        if (!record) {
+            return {
+                status: "error",
+                message: "Invalid or expired code"
+            }
+        }
+
+        if (record.expiresAt < new Date()) {
+            return {
+                status: "error",
+                message: "Code expired"
+            }
+        }
+
+        await prisma.verification.delete({
+            where: {
+                id: record.id
+            }
+        })
+
+        return {
+            status: "success",
+            message: "Code verified successfully"
+        }
+
+    } catch (error) {
+
+        console.log(error)
+        return {
+            status: "error",
+            message: "Failed to verify code"
+        }
+    }
+}
+
+export const resetPassword = async (email: string, code: string, newPassword: string) => {
+
+    try {
+
+        const record = await prisma.verification.findFirst({
+            where: {
+                identifier: email,
+                value: code
+            }
+        })
+
+        if (!record) {
+            return {
+                status: "error",
+                message: "Invalid or expired code"
+            }
+        }
+
+        if (record.expiresAt < new Date()) {
+            return {
+                status: "error",
+                message: "Code expired"
+            }
+        }
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10)
+
+        await prisma.account.updateMany({
+            where: {
+                user: { email }
+            },
+            data: {
+                password: hashedPassword
+            }
+        })
+
+        await prisma.verification.deleteMany({
+            where: { identifier: email }
+        })
+
+        return { success: true }
+
+    } catch (error) {
+        console.log(error)
+        return {
+            status: "error",
+            message: "Failed to verify code"
+        }
+    }
+}
--- lib/actions/user-actions.ts
+++ lib/actions/user-actions.ts
@@ -126,3 +126,4 @@ export async function updateUser(data: {
 
     }
 }
+
